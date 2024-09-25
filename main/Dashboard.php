19991db261800bf65f64a90c72f2f67651ed0bf4
elli0571 <elli0571@flinders.edu.au>
2024-09-25 15:15:01 +0930

    Create Dashboard.php

---
+++ main/Dashboard.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../Styles/Style.css">
+    <title>Dashboard</title>
+</head>
+<body>
+    <header>
+        <?php include '../inc/header.inc.php';?>
+        <h1>Dashboard</h1>
+    </header>
+    
+    <main>
+
+
+    </main>
+</body>
+</html>

e11a125c2357b2de20aca9620aa7123da9dd4d23
elli0571 <elli0571@flinders.edu.au>
2024-10-03 17:29:49 +0930

    added auditor pages and basic backend for summary report

---
+++ Auditor/Home Page.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <meta name="author" content="Ben" />
+    <title>Home Page</title>
+</head>
+<body>
+    <header>
+        <h1>Welcome to the Smart Manufactoring Dashboard</h1>
+        <h3>Role: Auditor</h3>
+        <p></p>
+    </header>
+    <main>
+        <button>Summary Report</button>
+        <p></p>
+        <button>Dashboard</button>
+        <p></p>
+        <button>Logout</button>
+
+      
+    </main>
+    <footer>
+    </footer>    
+</body>
+</html>
---
+++ Auditor/Summary Report.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="author" content="Ben">
+    <link rel="stylesheet" href="../Styles/Style.css">
+    <link rel="stylesheet" href="../Styles/Production Operator.css">
+    <title>Summary Report</title>
+</head>
+
+<main>
+
+<form method="POST">
+    <label for="start_date">Start Date:</label>
+    <input type="datetime-local" id="start_date" name="start_date" required>
+    
+    <label for="end_date">End Date:</label>
+    <input type="datetime-local" id="end_date" name="end_date" required>
+
+    <input type="submit" name="submit" value="Fetch Data">
+    <br></br>
+</form>
+
+<?php
+//fetch logs from logs
+require_once "../inc/dbconn.inc.php";
+
+// Check if the form is submitted
+if (isset($_POST['start_date']) && isset($_POST['end_date'])) {
+    // Get the start and end dates from the form
+    $start_date = $_POST['start_date'];
+    $end_date = $_POST['end_date'];
+
+
+    $sql = "SELECT * 
+    FROM `Factory Logs`
+    where 'timestamp' between '$start_date' and '$end_date' ";
+    if ($result = mysqli_query($conn, $sql)) {
+        if (mysqli_num_rows($result) >= 1 ) {
+            
+            echo "<ul>";
+            while ($row = mysqli_fetch_assoc($result)) {
+                echo "<li>" . implode(" - ", $row) . "</li>";
+            } echo "</ul>";
+            mysqli_free_result($result);
+
+        } else {
+            echo "No records found.";
+        }
+    }
+}
+mysqli_close($conn);
+?>
